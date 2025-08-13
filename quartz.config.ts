b7d86f128762fe72f459cb81497b079dc91c54b6
Bromtus <maxpieper01@gmail.com>
2025-08-13 22:53:06 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Colossus of the Drylands",
+    pageTitle: "Quartz 4",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
